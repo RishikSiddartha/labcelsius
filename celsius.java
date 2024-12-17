40beeb420edfcf0e71be57351e791d38635ceb6a
RishikSiddartha <rishiksiddartha2005@gmail.com>
2024-12-17 14:52:51 +0530

    simple celsius program

---
+++ celsius.java
@@ -0,0 +1,9 @@
+public class celsius{
+	public static void main (String args[]){
+		float fahrenheit,celsius;
+		fahrenheit=43;
+		celsius=((fahrenheit-32)*5)/9;
+		System.out.println("temp:"+celsius);
+	}
+}
+
